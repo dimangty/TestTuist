4d91ac6b541866ff8b3eba617b4a3605f733136d
dimangty <dimangty@mail.ru>
2025-10-20 12:58:31 +0700

    First Commit

---
+++ TestingTask/Core/Sources/MainScreen/Router/MainScreenRouter.swift
@@ -0,0 +1,37 @@
+//
+//  MainScreenRouter.swift
+//  TestingTask
+//
+//  Created by DBykov on 19/07/2022.
+//
+
+import UIKit
+
+class MainScreenRouter {
+    weak var viewController: UIViewController?
+    weak var transitionHandler: ViperModuleTransitionHandler?
+
+    func createModule() -> UIViewController? {
+        let vc = MainScreenViewController.loadFromXib()
+
+        transitionHandler = vc
+
+        let presenter = MainScreenPresenter(view: vc, router: self)
+
+        vc.presenter = presenter
+
+        self.viewController = vc
+
+        return self.viewController
+    }
+
+    func openModule(with transitionHandler: ViperModuleTransitionHandler, transitionStyle: TransitionStyle) {
+        if let vc = createModule() {
+            transitionHandler.openModule(vc: vc, style: transitionStyle)
+        }
+    }
+}
+
+extension MainScreenRouter: MainScreenRouterInput {
+
+}
